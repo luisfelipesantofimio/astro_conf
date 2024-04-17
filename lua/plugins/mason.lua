5a718e05257749209937ebba374d9b68a502775a
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2024-04-16 19:56:14 -0500

    Added some Python static analysis tools

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,6 +10,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "ruff",
+        "mypy",
         -- add more arguments for adding more language servers
       })
     end,
